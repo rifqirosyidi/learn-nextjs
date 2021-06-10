fe38cfd4de07c3ed3f203c7d993f77049a49c984
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2021-06-10 16:53:06 +0700

    layouts and css module

---
+++ components/Layout.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "../styles/Layout.module.css";
+
+const Layout = ({ children }) => {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1>Hello</h1>
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
